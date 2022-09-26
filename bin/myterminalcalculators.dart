c650d375fb8e252cc8528c83db8f7ad9c90410cb
vijayraj9526 <vijayraj9526@gmail.com>
2022-09-26 17:39:17 +0530

    feature/CAL-101 calculator coded

--- bin/myterminalcalculators.dart
+++ bin/myterminalcalculators.dart
@@ -1,3 +1,32 @@
+import 'dart:io';
+
 void main(List<String> arguments) {
-  print('Hello world!');
+  print('Welcome to My Terminal Calculator!');
+  print("Enter your first number");
+
+  int? num1 = int.parse(stdin.readLineSync()!);
+  print("enter aan operator (+,_,/,%,*)");
+  String? op = stdin.readLineSync()!.trim();
+
+  print("enter second number");
+  int? num2 = int.parse(stdin.readLineSync()!);
+
+  if (op != "" || ["+", "-", "/", "%", "*"].contains(op)) {
+    if (op == "+") {
+      print(num1 + num2);
+    } else if (op == "-") {
+      print(num1 - num2);
+    } else if (op == "/") {
+      print(num1 / num2);
+    } else if (op == "%") {
+      print(num1 % num2);
+    } else if (op == "*") {
+      print(num1 * num2);
+    }
+    else {
+    print("please select a valid operrator");
+  }
+  } else {
+    print("please select a valid operrator");
+  }
 }
